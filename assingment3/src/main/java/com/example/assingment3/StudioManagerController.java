efa4119e03f3a7c9afdea58f0136556fa9cce38e
Mena S. Youssef <msy32@crayon.cs.rutgers.edu>
2024-03-19 19:55:22 -0400

    update

--- assingment3/src/main/java/com/example/assingment3/StudioManagerController.java
+++ assingment3/src/main/java/com/example/assingment3/StudioManagerController.java
@@ -8,6 +8,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.time.LocalDate;
 
@@ -17,21 +23,32 @@ public class StudioManagerController {
     private Label guestPassLabel, errorMsg;
 
     @FXML
-    private RadioButton basicButton, familyButton, premiumButton;
+    private RadioButton basicButton, familyButton, premiumButton, LoadScheduleButton;
 
     @FXML
-    private RadioButton studioBridgewater, studioEdison, studioFranklin, studioPiscataway, studioSomerville;
+    private RadioButton studioBridgewater, studioEdison, studioFranklin, studioPiscataway, studioSomerville,
+    classBridgewater, classEdison, classFranklin, classPiscataway, classSomerville;
 
     @FXML
     private TextField firstName, lastName;
 
     @FXML
     private DatePicker dob;
-
-
+    @FXML
+    private Schedule schedule;
 
     private String fname, lname, strDob, studio, membershipType;
 
+    public void load(File file) throws IOException {
+        final int NOT_FOUND = -1;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                StringTokenizer tokens = new StringTokenizer(line, " ");
+                parseTokens(tokens);
+            }
+        }
+    }
 
 
     public void memberButtonClick(ActionEvent event) {
@@ -44,6 +61,7 @@ public class StudioManagerController {
         } else if (premiumButton.isSelected()) {
             guestPassLabel.setText("3");
             membershipType = "premium";
+
         }
     }
 
@@ -84,4 +102,13 @@ public class StudioManagerController {
         }
     }
 
-}
+        public void LoadScheduleClick(ActionEvent event) throws IOException {
+            Stage stage = (Stage)
+
+            FileChooser fileChooser = new FileChooser();
+            File selectedFile = fileChooser.showOpenDialog(schedule.(stage))
+            if (LoadScheduleButton.isSelected()) {
+
+            }
+        }
+    }
